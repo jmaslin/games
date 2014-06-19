8a428d0145af4b7bff0c7c75f87bf7a5d25e6958
Justin <me@justinmaslin.com>
2014-06-19 10:57:10 -0400

    Update

--- index.php
+++ index.php
@@ -7,20 +7,29 @@ require_once(TEMPLATES_PATH . "/header.php");
 
 <div class="row">
 
-	<div class="col-sm-6">
+	<div class="col-sm-4 col-sm-offset-4 text-center">
 		<h1>Home
 			<small>Welcome.</small>
 		</h1>
+		<hr>
 	</div>
 
 </div>
+
 
 <div class="row">
 
-	<div class="col-sm-8">
+	<div class="col-sm-6 col-sm-offset-3 text-center">
 		<p class="lead">
 			<?php include 'text.php'; ?>
 		</p>
+
+		<blockquote>
+		<p>This website is the best thing since sliced bread.</p>
+		<footer>Everyone in the world.</footer>
+		</blockquote>
 	</div>
 
 </div>
+
+<?php require_once(TEMPLATES_PATH . "/footer.php"); ?>
